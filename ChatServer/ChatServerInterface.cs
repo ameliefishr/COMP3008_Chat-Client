d5e307e95d57bb2dd91daf1969f8525ad9c0539f
jsonNotHere <20172772@student.curtin.edu.au>
2023-09-17 21:17:27 +0800

    server interface for login created

--- ChatServer/ChatServerInterface.cs
+++ ChatServer/ChatServerInterface.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ChatServer
 {
-    internal interface ChatServerInterface
+    [ServiceContract]
+    public interface ChatServerInterface
     {
+        [OperationContract]
+        void login(string username);
     }
 }
