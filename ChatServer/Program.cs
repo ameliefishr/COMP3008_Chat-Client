37556e6e8d80e7a4ca473eb6933555537a71da82
jsonNotHere <20172772@student.curtin.edu.au>
2023-09-17 22:19:03 +0800

    chat server initialised

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using InterfaceLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,18 @@ namespace ChatServer
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the Chat Server");
+
+            ServiceHost host;
+            NetTcpBinding tcp = new NetTcpBinding();
+
+            host = new ServiceHost(typeof(ChatServerImplement));
+            host.AddServiceEndpoint(typeof(ChatServerInterface), tcp, "net.tcp://0.0.0.0:8100/ChatService");
+
+            host.Open();
+            Console.WriteLine("System Online");
+            Console.ReadLine();
+            host.Close();
         }
     }
 }
