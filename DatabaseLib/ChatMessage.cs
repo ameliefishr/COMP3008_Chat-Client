3778de8182ca393bee5924e02ee5f7eb58752bc8
ameliefisher <amelie.fisher@student.curtin.edu.au>
2023-09-18 23:14:22 +0800

    yes

---
+++ DatabaseLib/ChatMessage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DatabaseLib
+{
+    [DataContract]
+    public class ChatMessage
+    {
+        [DataMember]
+        public string SenderUsername { get; set; } 
+
+        [DataMember]
+        public string Content { get; set; } 
+
+        [DataMember]
+        public DateTime Timestamp { get; set; } 
+
+        public ChatMessage(string senderUsername, string content)
+        {
+            SenderUsername = senderUsername;
+            Content = content;
+            Timestamp = DateTime.Now; // sets to curent time/date
+        }
+    }
+}
